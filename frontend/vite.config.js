07c5b4f614376c112733c31bab0527a8b6ab4912
nayan0402 <120306074+nayan0402@users.noreply.github.com>
2024-09-20 20:23:02 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,10 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      external: ['axios']
-    } // Ensure this is the correct output directory
-  },
+  base: '/', // or '/your-subdirectory/' if deployed in a subfolder
 });
