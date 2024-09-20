d63aad66e6fade2b8fb7567d41e5003916f9238d
nayan0402 <120306074+nayan0402@users.noreply.github.com>
2024-09-20 20:19:18 +0530

    Included axios in vite.confg.js for smooth deployement

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // Ensure this is the correct output directory
+    outDir: 'dist',
+    rollupOptions: {
+      external: ['axios']
+    } // Ensure this is the correct output directory
   },
 });
